46c3d817f6da4aafad201ba0dbdb1a499457dbcf
sanfilip <sanfilip@gmail.com>
2017-12-29 16:54:18 -0500

    reduce sleep delay; change sample to fox

--- run.sh
+++ run.sh
@@ -4,22 +4,22 @@
 add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
 apt update 2>&1
 apt install -y apt-utils 2>&1
-sleep 60
+sleep 10
 apt install -y ffmpeg 2>&1
-sleep 60
+sleep 10
 apt install -y libav-tools 2>&1
-sleep 60
+sleep 10
 apt install -y x264 2>&1
-sleep 60
+sleep 10
 apt install -y x265 2>&1
-sleep 60
+sleep 10
 
 #create folders
 mkdir data
 mkdir data/bin
 
 #run style transfer on video
-python transform_video.py --in-path sample.mp4 \
+python transform_video.py --in-path examples/content/fox.mp4 \
   --checkpoint ./scream.ckpt \
   --out-path /artifacts/out.mp4 \
   --device /gpu:0 \
