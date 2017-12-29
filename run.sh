37e88fe064959c2f8831ca10ae84ca151f23f3c8
sanfilip <sanfilip@users.noreply.github.com>
2017-12-29 16:11:00 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 #install ffmpeg to container
-add-apt-repository -y ppa:jonathonf/ffmpeg-3
-apt update && apt install -y ffmpeg libav-tools x264 x265
+add-apt-repository -y ppa:jonathonf/ffmpeg-3 2>&1
+apt update 2>&1
+apt install -y apt-utils 2>&1
+sleep 60
+apt install -y ffmpeg 2>&1
+sleep 60
+apt install -y libav-tools 2>&1
+sleep 60
+apt install -y x264 2>&1
+sleep 60
+apt install -y x265 2>&1
+sleep 60
 
 #create folders
 mkdir data
-cd data
-mkdir bin
+mkdir data/bin
 
 #run style transfer on video
-python transform_video.py --in-path edgeoftomorrow.mp4 \
+python transform_video.py --in-path sample.mp4 \
   --checkpoint ./scream.ckpt \
   --out-path /artifacts/out.mp4 \
   --device /gpu:0 \
-  --batch-size 4
+  --batch-size 4 2>&1
