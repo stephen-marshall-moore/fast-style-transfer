77a9c6350ddb424133dbbc8ee852a51307dd9bd8
Stephen Moore <stephen.moore@linux.com>
2019-05-12 08:57:11 -0700

    look for coco on /styletransfer

--- stylelize.sh
+++ stylelize.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 echo 'ciao, monde' > /artifacts/hello.txt
 pwd > /artifacts/working.txt
-ls -al >> /artifacts/working.txt
+ls -al /styletransfer >> /artifacts/working.txt
+ls -al /datasets >> /artifacts/working.txt
 mkdir /artifacts/checkpoints
 mkdir /artifacts/tests
 python style.py --style examples/style/rosemarys.jpg \
   --test examples/content/chicago.jpg \
   --checkpoint-dir /artifacts/checkpoints/ \
-  --vgg-path /data/imagenet-vgg-verydeep-19.mat \
-  --train-path /datasets/coco/ \
+  --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
+  --train-path /styletransfer/datasets/coco/ \
   --test-dir /artifacts/tests/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
