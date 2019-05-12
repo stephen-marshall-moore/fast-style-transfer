ac7c83f13d665d3bd92a8b015166cd1c840f3b67
Stephen Moore <stephen.moore@linux.com>
2019-05-12 07:55:11 -0700

    modify reference to artifacts path

--- stylelize.sh
+++ stylelize.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 ./setup.sh
-echo "ciao, monde" > artifacts/hello.txt
+echo 'ciao, monde' > /artifacts/hello.txt
 python style.py --style examples/style/rosemarys.jpg \
   --test examples/content/chicago.jpg
-  --checkpoint-dir artifacts \
+  --checkpoint-dir /artifacts \
   --content-weight 1.5e1 \
   --checkpoint-iterations 500 \
   --batch-size 20
