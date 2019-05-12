0131ea77c73cd1075fd80be7372151138c80bc48
Stephen Moore <stephen.moore@linux.com>
2019-05-12 08:05:22 -0700

    use data from docker image

--- stylelize.sh
+++ stylelize.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
-./setup.sh
 echo 'ciao, monde' > /artifacts/hello.txt
 python style.py --style examples/style/rosemarys.jpg \
   --test examples/content/chicago.jpg
-  --checkpoint-dir /artifacts \
+  --checkpoint-dir checkpoints/ \
+  --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
+  --train-path /datasets/coco/ \
+  --model-dir /artifacts \
+  --test-dir tests/ \
   --content-weight 1.5e1 \
-  --checkpoint-iterations 500 \
+  --checkpoint-iterations 1000 \
   --batch-size 20
