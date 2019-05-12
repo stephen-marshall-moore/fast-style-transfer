8d9ab4c9a87e93beabf7f3c4808badb4f20d8905
Stephen Moore <stephen.moore@linux.com>
2019-05-12 08:14:53 -0700

    create tests dir

--- stylelize.sh
+++ stylelize.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 echo 'ciao, monde' > /artifacts/hello.txt
 mkdir /artifacts/checkpoints
+mkdir /artifacts/tests
 python style.py --style examples/style/rosemarys.jpg \
   --test examples/content/chicago.jpg \
   --checkpoint-dir /artifacts/checkpoints/ \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/ \
-  --test-dir tests/ \
+  --test-dir /artifacts/tests/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
   --batch-size 20
