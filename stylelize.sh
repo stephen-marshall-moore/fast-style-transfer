fa846dcaa7b168424e02b025cfdace35942d706b
Stephen Moore <stephen.moore@linux.com>
2019-05-12 08:12:02 -0700

    no model-dir parameter

--- stylelize.sh
+++ stylelize.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 echo 'ciao, monde' > /artifacts/hello.txt
+mkdir /artifacts/checkpoints
 python style.py --style examples/style/rosemarys.jpg \
   --test examples/content/chicago.jpg \
-  --checkpoint-dir checkpoints/ \
+  --checkpoint-dir /artifacts/checkpoints/ \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/ \
-  --model-dir /artifacts \
   --test-dir tests/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
